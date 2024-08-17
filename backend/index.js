bf2260616c04fa3d84b901eff232891980ed31a5
Anmol Sangha <anmolsangha02@gmail.com>
2024-08-17 21:02:31 +0530

    make code deploy ready

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const routes = require("./route");
 
 const app = express();
 const cors = require("cors");
+const path = require('path');
 
 require("dotenv").config();
 require("./db/config");
@@ -10,6 +11,21 @@ require("./db/config");
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
+
+//deploy
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // Start the server
 const PORT = process.env.PORT || 4000;
